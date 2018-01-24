b8da81f675ac440a04c39878e0ef1c0cffd2f870
Ondra Chaloupka <ochaloup@redhat.com>
2018-01-24 21:05:00 +0100

    updating readmes and setup

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ export BYTEMAN_WORKSHOP="$PWD"
 
 
 # JAVA
-if [ ! -d "$BYTEMAN_WORKSHOP"/libs/jdk-*.tar.gz ]; then
-  [ ! -f "$BYTEMAN_WORKSHOP"/libs/jdk-*.tar.gz ] &&\
-    echo "Download the 'jdk8u161' from http://www.oracle.com/technetwork/java/javase/downloads/index-jsp-138363.html and place it to '$BYTEMAN_WORKSHOP/libs'" && return
+if [ ! -d "$BYTEMAN_WORKSHOP/libs/jdk1.8.0_161" ]; then
+   [ ! -f "$BYTEMAN_WORKSHOP/libs/jdk-8u161-linux-x64.tar.gz" ] &&\
+      echo "Download the 'jdk8u161' from http://www.oracle.com/technetwork/java/javase/downloads/index-jsp-138363.html and place it to '$BYTEMAN_WORKSHOP/libs'" && return
   tar -C "$BYTEMAN_WORKSHOP/libs" -xzf "$BYTEMAN_WORKSHOP"/libs/jdk-*.tar.gz
 fi
 
@@ -22,18 +22,28 @@ export PATH="$JAVA_HOME/bin:$PATH"
 
 # MAVEN
 if [ ! -d "$BYTEMAN_WORKSHOP/libs/apache-maven-3.5.2" ]; then
-  wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip -O $BYTEMAN_WORKSHOP/libs/maven352.zip
+   [ ! -f "$BYTEMAN_WORKSHOP/libs/maven352.zip" ]  &&\
+      wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip -O $BYTEMAN_WORKSHOP/libs/maven352.zip
   unzip -d "$BYTEMAN_WORKSHOP/libs" "$BYTEMAN_WORKSHOP/libs/maven352.zip"
 fi
 
 export MAVEN_HOME="$BYTEMAN_WORKSHOP/libs/apache-maven-3.5.2"
 export PATH="$MAVEN_HOME/bin:$PATH"
 
+# WILDFLY
+if [ ! -d "$BYTEMAN_WORKSHOP/libs/wildfly-11.0.0.Final" ]; then
+   [ ! -f "$BYTEMAN_WORKSHOP/libs/wildfly11.zip" ] &&\
+       wget http://download.jboss.org/wildfly/11.0.0.Final/wildfly-11.0.0.Final.zip -O "$BYTEMAN_WORKSHOP/libs/wildfly11.zip"
+   unzip -d "$BYTEMAN_WORKSHOP/libs" "$BYTEMAN_WORKSHOP/libs/wildfly11.zip"
+fi
+
+export JBOSS_HOME="$BYTEMAN_WORKSHOP/libs/wildfly-11.0.0.Final"
 
 # BYTEMAN
 if [ ! -d "$BYTEMAN_WORKSHOP/libs/byteman-download-4.0.0" ]; then
-  wget http://downloads.jboss.org/byteman/4.0.0/byteman-download-4.0.0-bin.zip -O "$BYTEMAN_WORKSHOP/libs/byteman400.zip"
-  unzip -d "$BYTEMAN_WORKSHOP/libs/" "$BYTEMAN_WORKSHOP/libs/byteman400.zip"
+   [ ! -f "$BYTEMAN_WORKSHOP/libs/byteman400.zip" ] &&\
+       wget http://downloads.jboss.org/byteman/4.0.0/byteman-download-4.0.0-bin.zip -O "$BYTEMAN_WORKSHOP/libs/byteman400.zip"
+   unzip -d "$BYTEMAN_WORKSHOP/libs/" "$BYTEMAN_WORKSHOP/libs/byteman400.zip"
 fi
 
 export BYTEMAN_HOME="$BYTEMAN_WORKSHOP/libs/byteman-download-4.0.0"
